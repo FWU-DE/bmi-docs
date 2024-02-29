84c61ae3a286b1dab317a5a7610b4b800b6d5927
Jan Renz <jan.renz@gmail.com>
2024-02-29 19:21:33 +0100

    try to enable github edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/fwu-de/bmi-docs/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/fwu-de/bmi-docs/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/fwu.de/bmi-docs/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //  'https://github.com/fwu.de/bmi-docs/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: './src/css/custom.css',
