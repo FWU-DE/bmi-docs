fe144bb64fe1f8fb75e02ef77cf6936461238ea5
Jan Renz <jan.renz@gmail.com>
2024-02-29 19:39:35 +0100

    Fixed typo in title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'FWI BMI Docs',
+  title: 'FWU BMI Docs',
   tagline: 'Technische Dokumentation der Bildungsmedieninfrastruktur des FWU',
   favicon: 'img/favicon.ico',
 
