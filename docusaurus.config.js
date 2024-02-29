e75b64f253b36c3607a23ddd339d15eb421ddb8d
Jan Renz <jan.renz@gmail.com>
2024-02-29 19:18:49 +0100

    removed fWU from title cause its in the logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,9 +66,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'FWU BMI Docs',
+        title: 'BMI Docs',
         logo: {
-          alt: 'Logo',
+          alt: 'LFWU Logo',
           src: 'img/logo.svg',
         },
         items: [
